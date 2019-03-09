d78589e695db5e208ec6aeecb7f4ab70bc2259ba
Lyud <jaysonc003@gmail.com>
2019-03-09 20:49:52 +0800

    Added CSS elements within the file

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Logo from './chaldea.png';
+import './app.css';
 
 const App = () => {
     return(
-        <div>
+        <div id="chaldeas">
             <img src={Logo} height="500" width="500"></img>
+            <p>Chaldea Security Organization</p>
         </div>
     )
 }
