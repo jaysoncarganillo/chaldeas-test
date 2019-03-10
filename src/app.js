be4fda5a7cbbca037e24dd81586e92d45a5501a2
Lyud <jaysonc003@gmail.com>
2019-03-10 21:50:33 +0800

    Added Card Section

--- src/app.js
+++ src/app.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ScrollableAnchor from 'react-scrollable-anchor';
 import Logo from './chaldea.png';
+import Card from './card-temp.png';
 import './app.css';
 
 const App = () => {
     return(
-        <div id="chaldeas">
-            <img src={Logo} height="500" width="500"></img>
-            <p>Chaldea Security Organization</p>
+        <div id="main">
+            <div class="main" id="chaldeas">
+                <img src={Logo} height="500" width="500"></img>
+                <p>Chaldea Security Organization</p>
+                <a href="#section1">Master ID</a>
+            </div>
+            <ScrollableAnchor id="section1">
+                <div class="main" id="card">
+                    <img src={Card}></img>
+                </div>
+            </ScrollableAnchor>
         </div>
     )
 }
