ca48d02ae41789a6b30f3e5e0709fed8f95fcdf1
Lyud <jaysonc003@gmail.com>
2019-03-11 21:20:21 +0800

    Added down arrow with blinking effects

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import ScrollableAnchor from 'react-scrollable-anchor';
 import Logo from './chaldea.png';
 import Card from './card-temp.png';
+import Down from './downarrow.png';
 import './app.css';
 
 const App = () => {
@@ -11,7 +12,7 @@ const App = () => {
             <div class="main" id="chaldeas">
                 <img src={Logo} height="500" width="500"></img>
                 <p>Chaldea Security Organization</p>
-                <a href="#section1">Master ID</a>
+                <a href="#section1"><img id="down" src={Down} height="100" width="100"></img></a>
             </div>
             <ScrollableAnchor id="section1">
                 <div class="main" id="card">
